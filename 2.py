171111eeb411c64aae1712450a50236cd9f36848
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-04 08:56:41 +0630

    second_lesson

---
+++ 2.py
@@ -0,0 +1,32 @@
+#variable
+#int a = 20 
+#string a = "khine"
+x = "2"
+#assignment operator
+#relational operator 1==1
+print(x,type(x))
+#while for True False is in try pass break except lambda global if else
+
+#data type 5 မျိုးရှိ
+#1.Numbers => 3 မျိုး integers float complex 
+#integers => 2 မျိုး signed int & long int
+#222222222222222222222222222222222222222222222222222222L
+#Hexadecimal value x = 0xABCDEFL
+#2.String "NCC" ''
+#3.List(array)
+#4.Tuple
+#5.Dictionary (key:value ပုံစံသိမ်း)
+
+
+#complex => real + imaginary number => 1+2j
+numberA = 1+2j
+print(numberA)
+print(numberA.real)#1
+print(numberA.imag)#2j
+
+number_b = complex(1,3)
+print(number_b)
+print(number_b.real)#1
+print(number_b.imag)#2j
+
+
