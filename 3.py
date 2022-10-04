610abfba39604f71f1bc51f8af69ba3dac1085ae
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-04 08:57:05 +0630

    third_lesson

---
+++ 3.py
@@ -0,0 +1,10 @@
+#standard input
+data = int(input("Please enter your age: "))
+print("User Input is: ",data)
+print("User Input Data Type is:",type(data))
+
+def function():
+    print("Hello")
+    print("Hello")
+    
+print("Hello")
