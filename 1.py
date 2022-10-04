90ee4188a9c67d1d426ac5b30a5217b76701192b
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-04 08:56:13 +0630

    first_lesson

---
+++ 1.py
@@ -0,0 +1,24 @@
+#standard output
+
+print("{1} Hello {2} World {0}!!".format("NCC",123,"Class"))
+#array 0 1 2 3 4
+
+#() {} 
+
+#NCC Hello Python World Class!!
+
+#keyword argument
+print("{name} Hello World!!{school} {org}".format(name="Khine",org="NCC",school="TTU"))
+
+name = "Oo"
+age = 24
+print(f'Hello I am {name} {age}')
+
+#float
+a = 1.34567
+print("The value of a is %.3f"%a)
+
+number = "123"
+str1 = "Khine"
+print(number+str1)
+print(type(number))
