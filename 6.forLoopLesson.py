c9ac97ea7454c8b297ff6915488ff9a3c2675715
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-10 22:06:21 +0630

    Create 6.forLoopLesson.py

---
+++ 6.forLoopLesson.py
@@ -0,0 +1,38 @@
+#for loop
+#for(int i=0;i<n;i++){
+#}
+# i = 0
+# while i<5:
+# 	print(i)
+# 	i+=1
+
+#for loop with range
+# z = range(5)
+# for i in z:#default 0
+# 	print(i)
+
+#for loop with list
+list1 = [11,12,13,14,15]
+list2 = ["k","i","o","p"]
+#          0  1   2   3
+for i in list2:
+	print(i)
+print("____________________________")
+#for loop with string
+str = "ncc"
+for i in "python":
+	print(i)
+print("____________________________")
+#for loop with tuple
+k = ('a','b','c',1,2,3,4)
+for i in k:
+	print(i)
+
+#for loop with list tuple
+lt = [(1,2),(3,4),(5,6),(9,8),(0,6)]
+    #   0     1     2     3     4
+for i,j in lt:
+	print(i,j)
+
+#လက်သည်းကွင်းပါတာကို packing လုပ်ထားတယ်လို့ခေါ်တယ်
+#လက်သည်းကွင်းပါတာကို un-packing လုပ်ထားတယ်လို့ခေါ်တယ်
