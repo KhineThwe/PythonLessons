4ee27d95603ab729f424ceb6aed882d8e6fcc731
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-11 22:06:24 +0630

    Create 4.TypeConversion.py

---
+++ 4.TypeConversion.py
@@ -0,0 +1,18 @@
+#Data Type Conversion
+f = 24.5
+g = int(f)
+print(type(f))
+print(g,type(g))
+
+str1 = "123"
+a = int(str1)
+print(type(str1))
+print(a,type(a))
+# int ==> float()
+# str()
+
+
+# str2 = "Hello"
+# b = int(str2)
+# print(type(str2))
+# print(b,type(b))
