003801220d2084afbddfd26aa29cbcde17feec91
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-11 22:07:41 +0630

    Create 8.BitwiseOpOr.py

---
+++ 8.BitwiseOpOr.py
@@ -0,0 +1,13 @@
+#Bitwise Op Or |
+#(2.Bitwise(|)or----->0 | 0 = 0,0 | 1 = 1,1 | 0 = 1,1 | 1 = 1)
+#x y | xy(output)
+#0 0 | 0
+#0 1 | 1
+#1 0 | 1
+#1 1 | 1
+
+a = 60
+b = 13
+c = a | b
+print('Bitwise OR',c)
+#61
