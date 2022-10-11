750f7760405e87466fe96a276619f60d6b75f4d0
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-11 22:07:04 +0630

    Create 6LogicalOp.py

---
+++ 6LogicalOp.py
@@ -0,0 +1,17 @@
+#Logical Op in Python
+#1.Arithmetic Op
+#2.Relational Op
+#3.Logical Op(and , or , not)     & | !
+
+a = 10
+b = 19
+c = 3
+if a>b or c>a:
+	print("One condition or Both are True")
+else:
+	print("Both are false")
+
+x = True #True False
+print('not x is ',not x)
+y = False
+print('not y is ',not y)
