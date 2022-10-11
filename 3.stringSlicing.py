85971af4f442d486db1f57b31bce3bef0361aeed
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-11 22:06:04 +0630

    Create 3.stringSlicing.py

---
+++ 3.stringSlicing.py
@@ -0,0 +1,13 @@
+# first_name = "NCC"
+# last_name = "Python"
+# full_name = first_name + " " +last_name
+# # print(full_name)
+# print("Hello, "+full_name+" Nice to meet you!")
+my_str = "abcdefg"
+#         0123456
+#            -2-1
+print(my_str[3:])
+print(my_str[:5])
+print(my_str[0])
+print(my_str[:])
+print(my_str[-1])
