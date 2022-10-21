7bd3f5dcc33a52cf89e9c11a1dd873c30847f755
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-21 22:06:19 +0630

    Create 6membershipop.py

---
+++ 6membershipop.py
@@ -0,0 +1,10 @@
+#membership op
+#in,not in
+
+list1 = [1,2,3,4,5,'khine']
+list2 = [1,2,3,4,5]
+# print(99 not in list1)
+if 'hello' in list1:
+	print('hello is in list1')
+else:
+	print('hello is not in list 1')
