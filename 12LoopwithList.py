ea9331caca1f25a505d9bead4157500b9c50ae67
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-21 22:08:07 +0630

    Create 12LoopwithList.py

---
+++ 12LoopwithList.py
@@ -0,0 +1,13 @@
+fruitList = ['apple','banna','orange','watermelon']
+numberList = [1,2,3,4,5,88,99]
+for element in fruitList:
+	if(element == 'apple'):
+		print(element)
+		break
+	else:
+		print('not found apple')
+
+for e in numberList:
+	e+=3
+	print(e)
+	
