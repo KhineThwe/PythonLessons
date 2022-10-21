f7eb734e86c945fdce654765c19619f7930b82df
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-21 22:07:12 +0630

    Create 9ListLesson2.py

---
+++ 9ListLesson2.py
@@ -0,0 +1,15 @@
+#List ထဲမှ data ထုတ်
+#1.clear တန်ဖိုးတွေတစ်ပြိုင်ထဲဖြတ်ချင်ရင်သုံးတယ်
+#2.pop ====> value return pyan pay tal
+#3.remove
+numbers = [22,22,32,45,66,22]
+# numbers.clear()
+print(numbers)
+# x = numbers.pop()
+# print(f'Remove element is : {x}')
+
+colors = ["Red","Green","Blue","Yellow"]
+print(colors)
+
+count = numbers.count(22)
+print(count)
