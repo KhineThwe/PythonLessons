05c5ebbfc10754a71a243e175593068849afc962
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-26 07:54:12 +0630

    Create 9SetLesson.py

---
+++ 9SetLesson.py
@@ -0,0 +1,12 @@
+#add element
+#add method in set ----> add an element
+#unorder
+#add multiple elements
+#update methon can be used
+my_set = {1,2,3}
+# my_set.add(88)
+# # my_set.add(99)
+# my_set.add(100)
+#add list and set
+my_set.update([11,12,13,14],{1,3,4,7,8,0})
+print(my_set)
