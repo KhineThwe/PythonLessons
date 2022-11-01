0881b7491e24afbb51d9378d0f5179c7331a36d0
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-31 21:21:55 -0800

    Create 13DicLesson2.py

---
+++ 13DicLesson2.py
@@ -0,0 +1,9 @@
+#Change & Add in Dictionary
+#change or modify
+my_name_dict = {'name':'NCC','age':24,'city':'Yangon'}
+my_name_dict['name'] = 'Python'
+print(my_name_dict)
+
+#add item
+my_name_dict['job'] = 'IT'
+print(my_name_dict)
