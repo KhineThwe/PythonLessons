3200facda3d4e9f22c0d6fb2ef07ed67733a6dca
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-31 21:19:08 -0800

    Create 7TupleLast.py

---
+++ 7TupleLast.py
@@ -0,0 +1,8 @@
+my_tuple = ('a','p','p','l','e')
+
+#membership operator -----> in,not in
+print('a' in my_tuple)
+print('o' not in my_tuple)
+# name_list = ('Khine','Zar','Thwe')
+for name in ('Khine','Zar','Thwe'):
+	print("Hello",name)
