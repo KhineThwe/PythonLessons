4d193b434a8732fc9e698a42d7f24d4083a3959d
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-31 21:22:23 -0800

    Create 14DicLastRemoving.py

---
+++ 14DicLastRemoving.py
@@ -0,0 +1,15 @@
+#Removing elements from dictionary
+#pop
+#popitem
+#clear
+#del
+my_name_dict = {'name':'NCC','age':24,'city':'Yangon'}
+print(my_name_dict.pop('name'))#return particular value
+print(my_name_dict.popitem())#
+
+
+# #remove all items
+# print(my_name_dict.clear())
+
+del my_name_dict
+print(my_name_dict)
