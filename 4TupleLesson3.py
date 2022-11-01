d513e34eae81988b00a3ebe715867699d1aa5699
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-31 21:17:46 -0800

    Create 4TupleLesson3.py

---
+++ 4TupleLesson3.py
@@ -0,0 +1,10 @@
+#Accessing Tuple
+#1.Index ---> positive,negative,nested index
+#Slicing
+
+my_tuple = (1,2,3,'Khine',(3,4,5))
+print(my_tuple[2])
+print(my_tuple[-1])
+print(my_tuple[4][2])
+print(my_tuple[:])#=print(my_tuple)
+print(my_tuple[:3])
