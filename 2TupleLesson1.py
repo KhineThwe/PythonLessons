f1e12bb514fc4fe87bd3bd74ff0007174c9bc579
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-31 21:16:50 -0800

    Create 2TupleLesson1.py

---
+++ 2TupleLesson1.py
@@ -0,0 +1,12 @@
+#()
+#Order
+#List
+#Tuple
+
+my_tuple = (1,2,3,'Khine')
+print(my_tuple[3])
+
+#nested tuple
+#tuple,list
+my_nested_tuple = ("mouse",[8,5,6],(3,4,6,6))
+print(type(my_nested_tuple[2]))
