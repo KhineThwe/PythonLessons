481512236efc55bafc47153f850fd85a5a00b182
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-31 21:17:17 -0800

    Create 3TupleLesson2.py

---
+++ 3TupleLesson2.py
@@ -0,0 +1,13 @@
+my_tuple = (1,2,3,'Khine')
+print(my_tuple[-1])
+
+my_eg_tuple = ("hello")
+print(type(my_eg_tuple))#str
+
+#creating a tuple with one element
+my_eg_tuple = ("hello",)
+print(type(my_eg_tuple))#tuple
+
+#parentheses is optional
+my_eg_tuple = "hello",
+print(type(my_eg_tuple))#tuple
