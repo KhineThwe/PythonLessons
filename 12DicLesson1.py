22551e882cb3cbf2c2d7fef56ba1aeaa43df0380
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-10-31 21:21:14 -0800

    Create 12DicLesson1.py

---
+++ 12DicLesson1.py
@@ -0,0 +1,7 @@
+#Accessing data from dictionary
+my_dict = {1:['apple','orange'],2:'ball'}
+print(my_dict[2])
+
+my_name_dict = {'name':'NCC','age':24,'city':'Yangon'}
+print(my_name_dict['age'])
+print(my_name_dict.get('age'))
